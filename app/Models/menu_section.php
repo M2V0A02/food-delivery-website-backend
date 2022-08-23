8d548d0057fece13f776b67a83324f5294afb9c3
m2v0a02 <volodin.mixa2015@yandex.ru>
2022-08-23 16:16:20 +0500

    api для таблицы employees

--- app/Models/menu_section.php
+++ app/Models/menu_section.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class menu_section extends Model
+class Menu_section extends Model
 {
     use HasFactory;
 }
--- app/Models/order_dish.php
+++ app/Models/order_dish.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class order_dish extends Model
+class Order_dish extends Model
 {
     use HasFactory;
 }
